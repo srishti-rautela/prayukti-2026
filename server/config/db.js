cd71790619479cea45bbba5a7604a95a93ce835f
srishti-rautela <rautelasrishti13@gmail.com>
2026-07-01 12:45:42 +0530

    Connected production backend

--- server/config/db.js
+++ server/config/db.js
@@ -3,11 +3,14 @@ require("dotenv").config();
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,      // <-- Add this line
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+
     waitForConnections: true,
-    connectionLimit: 10
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 module.exports = pool;
